3db45e981554f06ed29c6041799678bc46f42329
Alexander Stärk <alexander.staerk@cgm.com>
2018-12-05 21:23:21 +0100

    fix tslint errors

--- src/background.ts
+++ src/background.ts
@@ -1,76 +1,77 @@
-'use strict'
+"use strict";
 
-import { app, protocol, BrowserWindow } from 'electron'
-import {
-  createProtocol,
-  installVueDevtools
-} from 'vue-cli-plugin-electron-builder/lib'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+import { app, protocol, BrowserWindow } from "electron";
+import { createProtocol, installVueDevtools } from "vue-cli-plugin-electron-builder/lib";
 
-// Keep a global reference of the window object, if you don't, the window will
+const isDevelopment: boolean = process.env.NODE_ENV !== "production";
+
+// keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win: BrowserWindow | null = null;
 
-// Standard scheme must be registered before the app is ready
-protocol.registerStandardSchemes(['app'], { secure: true })
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+// standard scheme must be registered before the app is ready
+protocol.registerStandardSchemes(["app"], { secure: true });
+
+function createWindow(): void {
+  // create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 });
 
   if (isDevelopment || process.env.IS_TEST) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    // load the url of the dev server if in development mode
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string);
+    if (!process.env.IS_TEST) {
+      win.webContents.openDevTools();
+    }
   } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
+    createProtocol("app");
+    // load the index.html when not in development
+    win.loadURL("app://./index.html");
   }
 
-  win.on('closed', () => {
-    win = null
-  })
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
+// quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // on macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
+app.on("activate", () => {
+  // on macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-// This method will be called when Electron has finished
+// this method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+// some APIs can only be used after this event occurs.
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    await installVueDevtools()
+    // install Vue Devtools
+    await installVueDevtools();
   }
-  createWindow()
-})
+  createWindow();
+});
 
-// Exit cleanly on request from parent process in development mode.
+// exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', data => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", data => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
