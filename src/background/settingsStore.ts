30d32b1b37663f4aa9950735744f0b110ddf488f
Alexander Stärk <alexander.staerk@cgm.com>
2018-12-09 14:36:24 +0100

    commented out concepts of settingsStore

--- src/background/settingsStore.ts
+++ src/background/settingsStore.ts
@@ -1,19 +1,43 @@
 import Settings from "./settings";
+import * as fs from "fs";
+import { app } from "electron";
 
 export default class SettingsStore {
-    constructor() {
-        //
-    }
+
+    // private settingsFilePath: string = app.getPath("appData") + "/Wallboard/settings.json";
+
+    // private settings: Promise<Settings> | null = null;
 
     public loadSettings(): Settings {
         return new Settings();
     }
 
-    public saveSettings(): void {
-        //
+    /*public loadSettings(): Promise<Settings> {
+        if (this.settings == null) {
+            this.settings = this.loadSettingsFromFile();
+        }
+        return this.settings;
+    }*/
+
+    public saveSettings(/*a: number*/): void /*Promise<void>*/ {
+        /*return new Promise((resolve, reject) => {
+            fs.writeFile(this.settingsFilePath, this.settings, (err: NodeJS.ErrnoException) => {
+                if (err) {
+                    console.error(err);
+                    reject();
+                    return;
+                }
+            });
+        });*/
     }
 
     public clearCache(): void {
-        //
+        // this.settings = null;
     }
+
+    /*private loadSettingsFromFile(): Promise<Settings> {
+        return new Promise<Settings>((resolve, reject) => {
+            if (fs.existsSync)
+        });
+    }*/
 }
